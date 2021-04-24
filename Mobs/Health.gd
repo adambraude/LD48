6df763414a657383cad19d1a2d6ac8b035e8db8e
adambraude <malfunctioningadamantium@gmail.com>
2021-04-24 10:05:38 -0700

    Add invulnerability flicker

--- Mobs/Health.gd
+++ Mobs/Health.gd
@@ -1,17 +1,30 @@
 extends Node2D
 
+signal startAnim
 
 # Declare member variables here. Examples:
 export var maxHealth = 100.0
 var currentHealth = maxHealth
+
+var invuln = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func takeDamage(damage):
+	if invuln:
+		return
 	print("taking " + str(damage) + " damage")
 	currentHealth -= damage;
 	if currentHealth < 0:
 		currentHealth = 0
 	$Bar.rect_scale = Vector2(currentHealth/maxHealth, 1)
+	invuln = true
+	$Invulnerability.start()
+	emit_signal("startAnim")
+
+
+func onInvulnTimerEnd():
+	invuln = false
+	pass # Replace with function body.
