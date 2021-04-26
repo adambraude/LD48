823a0cee55b68d2bc5aea8fb7e2804418f794262
ECorpron <ecorpron@gmail.com>
2021-04-25 23:04:25 -0700

    music toggle on or off now does so

--- Sandboxes/Test Music/Music Player.gd
+++ Sandboxes/Test Music/Music Player.gd
@@ -6,6 +6,7 @@ extends Node
 var index
 var children
 var rng
+var manualPause
 # var b = "text"
 
 
@@ -13,18 +14,33 @@ var rng
 func _ready():
 	#children = shuffleList(children)
 	rng = RandomNumberGenerator.new()
+	manualPause = false
 	playSong()
 
 func song_finished():
 	playSong()
 
+# Just picks a random song from the list. I tried to have it cycle through all
+# of the songs first, but couldn't seem to get it to work
 func playSong():	
-	print(get_child_count())
+	#print(get_child_count())
+	if manualPause:
+		return
 	rng.randomize()
+	
+	index = rng.randi_range(0, get_child_count()-1)
 
-	var song = get_child(rng.randi_range(0, get_child_count()-1))
+	var song = get_child(index)
 	song.play()
 
+func pause():
+	manualPause = true
+	var song = get_child(index)
+	song.stop()
+
+func unpause():
+	manualPause = false
+	playSong()
 # Doesn't seem to work
 #func shuffleList(list):
 #	var shuffledList = [] 
